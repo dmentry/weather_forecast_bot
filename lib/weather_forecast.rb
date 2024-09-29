02cb5655cc34864a9094f7d87837bb2eb61c7b45
dack99 <slipin2020@mail.ru>
2024-09-29 14:27:12 +0300

    Cosmetic change

--- lib/weather_forecast.rb
+++ lib/weather_forecast.rb
@@ -92,9 +92,10 @@ class WeatherForecast
   def create_daily_forecast(forecast:, first_message: nil)
     return '' if !forecast
 
-    celsius = "\xE2\x84\x83"
+    # celsius = "\xE2\x84\x83"
+    celsius = "&#176;"
 
-    precipitation_volume =  if forecast[:preciptype]
+    precipitation_volume =  if forecast[:preciptype] && forecast[:preciptype].to_f > 0
                               case forecast[:preciptype]&.first
                               when 'rain'
                                 "<b>#{ forecast[:precip]&.to_f }мм</b>"
@@ -137,7 +138,7 @@ class WeatherForecast
     wind           = "Ветер:              <b>#{ forecast[:windspeed].round }м/с #{ wind_direction(forecast[:winddir]) }</b>#{ wind_gust }"
     cloudness      = "Облачность:    <b>#{ forecast[:cloudcover].to_i }%</b>"
     weather_descr  = "#{ forecast[:description] }"
-    precipitation2 += if forecast[:preciptype]
+    precipitation2 += if (forecast[:preciptype] && forecast[:precipprob]).to_f > 0) || precipitation_volume
                        " вероятность осадков: <b>#{ (forecast[:precipprob]).to_f }%</b>, выпадет #{ precipitation_volume }"
                      else
                        " осадков не ожидается"
